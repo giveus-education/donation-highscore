27bc1d46c138e2e36d770a1b8722e9331959da83
Jens <jens@dieburkards.de>
2020-09-19 12:32:29 +0200

    Inital stripe react commit with simple checkout form

--- packages/ui/app/src/App.tsx
+++ packages/ui/app/src/App.tsx
@@ -1,25 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Elements} from "@stripe/react-stripe-js";
+import  {loadStripe} from "@stripe/stripe-js";
+import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
+
+
+
+const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event:any) => {
+        // Block native form submission.
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            // Stripe.js has not loaded yet. Make sure to disable
+            // form submission until Stripe.js has loaded.
+            return;
+        }
+
+        // Get a reference to a mounted CardElement. Elements knows how
+        // to find your CardElement because there can only ever be one of
+        // each type of element.
+        const cardElement = elements.getElement(CardElement);
+
+        // Use your card Element with other Stripe.js APIs
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card: cardElement!,
+        });
+
+        if (error) {
+            console.log('[error]', error);
+        } else {
+            console.log('[PaymentMethod]', paymentMethod);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement />
+            <button type="submit" disabled={!stripe}>
+                Pay
+            </button>
+        </form>
+    );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Elements stripe={stripePromise}>
+          <CheckoutForm />
+      </Elements>
   );
 }
 
