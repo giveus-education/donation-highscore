aee8c143e781819691246f256a0777e726392176
Michael Bahr <michael.bahr@live.de>
2020-09-19 12:01:57 +0200

    Init react app

--- packages/ui/cdk/lib/donation-app-frontend.ts
+++ packages/ui/cdk/lib/donation-app-frontend.ts
@@ -5,7 +5,7 @@ export class DonationAppFrontend extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new SPADeploy(this, 'spaDeploy').createBasicSite({indexDoc: 'index.html', websiteFolder: '../app/dist'});
+    new SPADeploy(this, 'spaDeploy').createBasicSite({indexDoc: 'index.html', websiteFolder: '../app/build'});
 
     // The code that defines your stack goes here
   }
