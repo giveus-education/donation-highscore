25c63df63b6c78fcb2434c72e977dc2caae1808b
Michael Bahr <michael.bahr@live.de>
2020-09-19 13:06:15 +0200

    Add stripe checkout redirect

--- packages/ui/app/src/App.tsx
+++ packages/ui/app/src/App.tsx
@@ -1,60 +1,39 @@
 import React from 'react';
 import './App.css';
-import {Elements} from "@stripe/react-stripe-js";
 import  {loadStripe} from "@stripe/stripe-js";
-import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
 
 const stripePromise = loadStripe('pk_test_51HT23CJ2Y4J7zMet8sP3OiAuslVEJF1UkZk0HKOyiSDG7411mNGz5T7MBF6PJsHtElF8fsA99rzD8KhwgYYgKoRk00DdCr5OO4');
 
+const handleClick = async (event: any) => {
+    // Get Stripe.js instance
+    const stripe = await stripePromise;
 
+    const data = {amount: 2000};
 
-const CheckoutForm = () => {
-    const stripe = useStripe();
-    const elements = useElements();
+    // Call your backend to create the Checkout Session
+    const response = await fetch('https://gx5jzd1ua2.execute-api.us-east-1.amazonaws.com/dev/checkoutsession', { method: 'POST', body: JSON.stringify(data) });
 
-    const handleSubmit = async (event:any) => {
-        // Block native form submission.
-        event.preventDefault();
+    const session = await response.json();
 
-        if (!stripe || !elements) {
-            // Stripe.js has not loaded yet. Make sure to disable
-            // form submission until Stripe.js has loaded.
-            return;
-        }
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe!.redirectToCheckout({
+      sessionId: session.id,
+    });
 
-        // Get a reference to a mounted CardElement. Elements knows how
-        // to find your CardElement because there can only ever be one of
-        // each type of element.
-        const cardElement = elements.getElement(CardElement);
-
-        // Use your card Element with other Stripe.js APIs
-        const {error, paymentMethod} = await stripe.createPaymentMethod({
-            type: 'card',
-            card: cardElement!,
-        });
-
-        if (error) {
-            console.log('[error]', error);
-        } else {
-            console.log('[PaymentMethod]', paymentMethod);
-        }
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <CardElement />
-            <button type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
-    );
-};
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+    }
+  };
 
 function App() {
   return (
-      <Elements stripe={stripePromise}>
-          <CheckoutForm />
-      </Elements>
+    <>
+        <button role="link" onClick={handleClick}>
+            Donate
+        </button>
+    </>
   );
 }
 
